da914aae87475bd8940891007de08a8827b8499d
abhi <resnovatechnologies@gmail.com>
2017-08-30 22:37:21 +0530

    enter js in textbox

---
+++ abc.php
@@ -0,0 +1,11 @@
+
+
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+Name: <input type="text" name="name" value="<?php echo $name;?>">
+<input type="submit" name="submit" value="Submit">
+</form>
+<?php
+echo $_POST["name"];
+
+?>
